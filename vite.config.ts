98da20fb20351644c40cb9918ba3d8e489a54821
Lyove <lyove@outlook.com>
2023-07-03 17:43:36 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,35 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    port: 3030,
-  },
-  resolve: {
-    alias: {
-      "@/*": resolve(__dirname, "src"),
+export default defineConfig(({ command, mode }) => {
+  // when command line: vite
+  if (command === "serve") {
+    // do something
+  }
+  // when command line: vite build
+  else if (command === "build") {
+    // do something
+    // fs.rmdirSync("./dist", { recursive: true });
+  }
+
+  // such as command line: vite --mode development
+  if (mode === "development") {
+    // do something
+  }
+  // such as command line: vite build --mode production
+  else if (mode === "production") {
+    // do something
+  }
+  
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      port: 3030,
     },
-  },
+    resolve: {
+      alias: {
+        "@/*": resolve(__dirname, "src"),
+      },
+    },
+  }
 });
