36a439d634abe2d13f9f949e0000ea021a5fc7ee
Lyove <lyove@outlook.com>
2023-12-05 17:47:50 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import packageJson from "./package.json";
+
+const packageName = packageJson.name;
 
 const getPackageNameCamelCase = () => {
   try {
-    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
+    return packageName.replace(/-./g, (char) => char[1].toUpperCase());
   } catch (err) {
     throw new Error("Name property in package.json is missing.");
   }
